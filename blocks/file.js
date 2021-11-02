a6383771404ecc291abe1008e40a2bac31932610
You-Cheng Xiao <jason53415@gmail.com>
2021-11-03 01:32:37 +0800

    Customize file blocks

---
+++ blocks/file.js
@@ -0,0 +1,130 @@
+/**
+ * @license
+ * Copyright 2021 PAIA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview File blocks for Blockly.
+ * @author jason53415@gmail.com (You-Cheng Xiao)
+ */
+'use strict';
+
+goog.provide('Blockly.Constants.File');
+
+goog.require('Blockly');
+goog.require('Blockly.Blocks');
+goog.require('Blockly.FieldTextInput');
+goog.require('Blockly.FieldDropdown');
+goog.require('Blockly.FieldLabel');
+
+Blockly.defineBlocksWithJsonArray([
+  // Block for saving an object as a pickle file.
+  {
+    "type": "file_save",
+    "message0": "%{BKY_FILE_SAVE}",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "OBJECT"
+      },
+      {
+        "type": "input_value",
+        "name": "FILENAME",
+        "check": "String"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "style": "file_blocks",
+    "tooltip": "%{BKY_FILE_SAVE_TOOLTIP}"
+  },
+  // Block for loading an object from a pickle file.
+  {
+    "type": "file_load",
+    "message0": "%{BKY_FILE_LOAD}",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "OBJECT"
+      },
+      {
+        "type": "input_value",
+        "name": "FILENAME",
+        "check": "String"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "style": "file_blocks",
+    "tooltip": "%{BKY_FILE_LOAD_TOOLTIP}"
+  },
+  // Block for saving an object as a csv file.
+  {
+    "type": "file_csv_save",
+    "message0": "%{BKY_FILE_CSV_SAVE}",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "OBJECT"
+      },
+      {
+        "type": "input_value",
+        "name": "FILENAME",
+        "check": "String"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "DELIMITER",
+        "options": [
+          [",", ","],
+          [":", ":"],
+          [";", ";"],
+          ["|", "|"],
+          ["SPACE", " "],
+          ["TAB", "\\t"]
+        ]
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "style": "file_blocks",
+    "tooltip": "%{BKY_FILE_SAVE_TOOLTIP}"
+  },
+  // Block for loading an object from a csv file.
+  {
+    "type": "file_csv_load",
+    "message0": "%{BKY_FILE_CSV_LOAD}",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "OBJECT"
+      },
+      {
+        "type": "input_value",
+        "name": "FILENAME",
+        "check": "String"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "DELIMITER",
+        "options": [
+          [",", ","],
+          [":", ":"],
+          [";", ";"],
+          ["|", "|"],
+          ["SPACE", " "],
+          ["TAB", "\\t"]
+        ]
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "style": "file_blocks",
+    "tooltip": "%{BKY_FILE_LOAD_TOOLTIP}"
+  }
+]);
---
+++ generators/python/file.js
@@ -0,0 +1,69 @@
+/**
+ * @license
+ * Copyright 2021 PAIA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Generating Python for file blocks.
+ * @author jason53415@gmail.com (You-Cheng Xiao)
+ */
+'use strict';
+
+goog.provide('Blockly.Python.File');
+
+goog.require('Blockly.Python');
+
+Blockly.Python['file_save'] = function(block) {
+  // Save an object as a pickle file.
+  Blockly.Python.definitions_['import_pickle'] = 'import pickle';
+  Blockly.Python.definitions_['import_os'] = 'import os';
+  var obj = Blockly.Python.valueToCode(block, 'OBJECT',
+      Blockly.Python.ORDER_NONE) || 'None';
+  var name = Blockly.Python.valueToCode(block, 'FILENAME',
+      Blockly.Python.ORDER_NONE) || ("'save_" + new Date().getTime() + "'");
+  var code = "with open(os.path.join(os.path.dirname(__file__), " + name + " + '.pickle'), 'wb') as f:\n" + 
+      Blockly.Python.prefixLines("pickle.dump(" + obj + ", f)\n", Blockly.Python.INDENT);
+  return code;
+};
+
+Blockly.Python['file_load'] = function(block) {
+  // Load an object from a pickle file.
+  Blockly.Python.definitions_['import_pickle'] = 'import pickle';
+  Blockly.Python.definitions_['import_os'] = 'import os';
+  var obj = Blockly.Python.valueToCode(block, 'OBJECT',
+      Blockly.Python.ORDER_NONE) || '_';
+  var name = Blockly.Python.valueToCode(block, 'FILENAME',
+      Blockly.Python.ORDER_NONE) || "''";
+  var code = "with open(os.path.join(os.path.dirname(__file__), " + name + " + '.pickle'), 'rb') as f:\n" + 
+      Blockly.Python.prefixLines(obj + " = pickle.load(f)\n", Blockly.Python.INDENT);
+  return code;
+};
+
+Blockly.Python['file_csv_save'] = function(block) {
+    // Save an object as a csv file.
+    Blockly.Python.definitions_['import_csv'] = 'import csv';
+    Blockly.Python.definitions_['import_os'] = 'import os';
+    var obj = Blockly.Python.valueToCode(block, 'OBJECT',
+        Blockly.Python.ORDER_NONE) || 'None';
+    var name = Blockly.Python.valueToCode(block, 'FILENAME',
+        Blockly.Python.ORDER_NONE) || ("'save_" + new Date().getTime() + "'");
+    var delimiter = block.getFieldValue('DELIMITER');
+    var code = "with open(os.path.join(os.path.dirname(__file__), " + name + " + '.csv'), 'w', newline='') as f:\n" + 
+        Blockly.Python.prefixLines("csv.writer(f, delimiter='" + delimiter + "').writerows(" + obj + ")\n", Blockly.Python.INDENT);
+    return code;
+  };
+  
+  Blockly.Python['file_csv_load'] = function(block) {
+    // Load an object from a csv file.
+    Blockly.Python.definitions_['import_csv'] = 'import csv';
+    Blockly.Python.definitions_['import_os'] = 'import os';
+    var obj = Blockly.Python.valueToCode(block, 'OBJECT',
+        Blockly.Python.ORDER_NONE) || '_';
+    var name = Blockly.Python.valueToCode(block, 'FILENAME',
+        Blockly.Python.ORDER_NONE) || "''";
+    var delimiter = block.getFieldValue('DELIMITER');
+    var code = "with open(os.path.join(os.path.dirname(__file__), " + name + " + '.csv'), 'r', newline='') as f:\n" + 
+        Blockly.Python.prefixLines(obj + " = [row for row in csv.reader(f, delimiter='" + delimiter + "')]\n", Blockly.Python.INDENT);
+    return code;
+  };
