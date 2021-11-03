54d0bd7110cd1c46c96d5565f8b771ee8f17ddf7
You-Cheng Xiao <jason53415@gmail.com>
2021-11-03 08:52:30 +0800

    Customize model blocks

---
+++ blocks/model.js
@@ -0,0 +1,417 @@
+/**
+ * @license
+ * Copyright 2021 PAIA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Model blocks for Blockly.
+ * @author jason53415@gmail.com (You-Cheng Xiao)
+ */
+'use strict';
+
+goog.provide('Blockly.Constants.Model');
+
+goog.require('Blockly');
+goog.require('Blockly.Blocks');
+goog.require('Blockly.FieldTextInput');
+goog.require('Blockly.FieldDropdown');
+goog.require('Blockly.FieldLabel');
+
+Blockly.defineBlocksWithJsonArray([
+  // Train a model.
+  {
+    "type": "model_train",
+    "message0": "%{BKY_MODEL_TRAIN}",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "X"
+      },
+      {
+        "type": "input_value",
+        "name": "Y"
+      },
+      {
+        "type": "input_value",
+        "name": "MODEL"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "style": "model_blocks",
+    "tooltip": "%{BKY_MODEL_TRAIN_TOOLTIP}"
+  },
+  // Use model to predict.
+  {
+    "type": "model_predict",
+    "message0": "%{BKY_MODEL_PREDICT}",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "MODEL"
+      },
+      {
+        "type": "input_value",
+        "name": "X"
+      }
+    ],
+    "inputsInline": true,
+    "output": null,
+    "style": "model_blocks",
+    "tooltip": "%{BKY_MODEL_PREDICT_TOOLTIP}"
+  },
+  // Evaluate the predicted classification results.
+  {
+    "type": "model_evaluate_classification",
+    "message0": "%{BKY_MODEL_EVALUATE_CLASSIFICATION}",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "TRUE"
+      },
+      {
+        "type": "input_value",
+        "name": "PRED"
+      }
+    ],
+    "inputsInline": true,
+    "output": null,
+    "style": "model_blocks",
+    "tooltip": "%{BKY_MODEL_EVALUATE_CLASSIFICATION_TOOLTIP}"
+  },
+  // Evaluate the predicted regression results.
+  {
+    "type": "model_evaluate_regression",
+    "message0": "%{BKY_MODEL_EVALUATE_REGRESSION}",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "TRUE"
+      },
+      {
+        "type": "input_value",
+        "name": "PRED"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "FUNC",
+        "options": [
+          ["%{BKY_MODEL_EVALUATE_R2}", "R2"],
+          ["%{BKY_MODEL_EVALUATE_MAE}", "MAE"],
+          ["%{BKY_MODEL_EVALUATE_MSE}", "MSE"]
+        ]
+      }
+    ],
+    "inputsInline": true,
+    "output": null,
+    "style": "model_blocks",
+    "tooltip": "%{BKY_MODEL_EVALUATE_REGRESSION_TOOLTIP}"
+  },
+  {
+    "type": "model_train_test_split",
+    "message0": "%{BKY_MODEL_TRAIN_TEST_SPLIT}",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "X"
+      },
+      {
+        "type": "input_value",
+        "name": "Y"
+      },
+      {
+        "type": "input_value",
+        "name": "TEST_SIZE",
+        "check": "Number"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "SHUFFLE",
+        "options": [
+          ["%{BKY_MODEL_SHUFFLE}", "True"],
+          ["%{BKY_MODEL_NO_SHUFFLE}", "False"]
+        ]
+      },
+      {
+        "type": "input_value",
+        "name": "TRAIN_DATA"
+      },
+      {
+        "type": "input_value",
+        "name": "TEST_DATA"
+      },
+      {
+        "type": "input_value",
+        "name": "TRAIN_TARGET"
+      },
+      {
+        "type": "input_value",
+        "name": "TEST_TARGET"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "style": "model_blocks",
+    "tooltip": "%{BKY_MODEL_TRAIN_TEST_SPLIT_TOOLTIP}"
+  },
+  {
+    "type": "model_k_fold",
+    "message0": "%{BKY_MODEL_K_FOLD}",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "X"
+      },
+      {
+        "type": "input_value",
+        "name": "Y"
+      },
+      {
+        "type": "input_value",
+        "name": "K",
+        "check": "Number"
+      },
+      {
+        "type": "field_dropdown",
+        "name": "SHUFFLE",
+        "options": [
+          ["%{BKY_MODEL_NO_SHUFFLE}", "False"],
+          ["%{BKY_MODEL_SHUFFLE}", "True"]
+        ]
+      }
+    ],
+    "inputsInline": true,
+    "output": null,
+    "style": "model_blocks",
+    "tooltip": "%{BKY_MODEL_K_FOLD_TOOLTIP}"
+  }
+]);
+
+Blockly.Blocks["model_create_classification"] = {
+  /**
+   * Block for creating classification model.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    var MODEL =
+        [
+          [Blockly.Msg['MODEL_KNN'], 'KNeighborsClassifier'],
+          [Blockly.Msg['MODEL_LINEAR_SVM'], 'LinearSVC'],
+          [Blockly.Msg['MODEL_DECISION_TREE'], 'DecisionTreeClassifier'],
+          [Blockly.Msg['MODEL_RANDOM_FOREST'], 'RandomForestClassifier'],
+          [Blockly.Msg['MODEL_MLP'], 'MLPClassifier'],
+          [Blockly.Msg['MODEL_SGD'], 'SGDClassifier']
+        ];
+    this.setStyle('model_blocks');
+    var modelMenu = new Blockly.FieldDropdown(MODEL, function(value) {
+      this.getSourceBlock().updateParameters_(value);
+    });
+    this.appendDummyInput()
+        .appendField(Blockly.Msg['MODEL_CREATE'])
+        .appendField(modelMenu, 'MODEL')
+        .appendField(Blockly.Msg['MODEL_CLASSIFICATION']);
+    this.setInputsInline(true);
+    this.setOutput(true);
+    this.setTooltip(Blockly.Msg['MODEL_CREATE_CLASSIFICATION_TOOLTIP']);
+    this.paramCount_ = 0;
+  },
+  updateParameters_: function(model) {
+    for (var x = 0; x < this.paramCount_; x++) {
+      this.removeInput('PARAM' + x);
+    }
+    switch (model) {
+      case 'KNeighborsClassifier':
+        this.appendDummyInput('PARAM0')
+            .appendField(" k :")
+            .appendField(new Blockly.FieldNumber(5, 1, 10000, 1), 'PARAM_K')
+            .appendField(" " + Blockly.Msg['MODEL_WEIGHT'] + " :")
+            .appendField(new Blockly.FieldDropdown([
+              [Blockly.Msg['MODEL_WEIGHT_UNIFORM'], 'uniform'],
+              [Blockly.Msg['MODEL_WEIGHT_DISTANCE'], 'distance']
+            ]), 'PARAM_WEIGHTS')
+            .appendField(" " + Blockly.Msg['MODEL_ALGORITHM'] + " :")
+            .appendField(new Blockly.FieldDropdown([
+              [Blockly.Msg['MODEL_ALGORITHM_AUTO'], 'auto'],
+              [Blockly.Msg['MODEL_ALGORITHM_BALL_TREE'], 'ball_tree'],
+              [Blockly.Msg['MODEL_ALGORITHM_KD_TREE'], 'kd_tree'],
+              [Blockly.Msg['MODEL_ALGORITHM_BRUTE'], 'brute'],
+            ]), 'PARAM_ALGORITHM');
+        this.paramCount_ = 1;
+        break;
+      case 'LinearSVC':
+        this.appendDummyInput('PARAM0')
+            .appendField(" " + Blockly.Msg['MODEL_C'] + " :")
+            .appendField(new Blockly.FieldNumber(1, 0.0001, null, 0.0001), 'PARAM_C')
+            .appendField(" " + Blockly.Msg['MODEL_PENALTY'] + " :")
+            .appendField(new Blockly.FieldDropdown([
+              [Blockly.Msg['MODEL_PENALTY_L2'], 'l2'],
+              [Blockly.Msg['MODEL_PENALTY_L1'], 'l1']
+            ]), 'PARAM_PENALTY')
+            .appendField(" " + Blockly.Msg['MODEL_LOSS'] + " :")
+            .appendField(new Blockly.FieldDropdown([
+              [Blockly.Msg['MODEL_LOSS_SQUARED_HINGE'], 'squared_hinge'],
+              [Blockly.Msg['MODEL_LOSS_HINGE'], 'hinge']
+            ]), 'PARAM_LOSS');
+        this.paramCount_ = 1;
+        break;
+      case 'DecisionTreeClassifier':
+        this.appendDummyInput('PARAM0')
+            .appendField(" " + Blockly.Msg['MODEL_MAX_DEPTH'] + " :")
+            .appendField(new Blockly.FieldNumber(5, 0, null, 1), 'PARAM_MAX_DEPTH')
+            .appendField(" " + Blockly.Msg['MODEL_MIN_SAMPLES_SPLIT'] + " :")
+            .appendField(new Blockly.FieldNumber(2, 2, null, 1), 'PARAM_MIN_SAMPLES_SPLIT');
+        this.paramCount_ = 1;
+        break;
+      case 'RandomForestClassifier':
+        this.appendDummyInput('PARAM0')
+            .appendField(" " + Blockly.Msg['MODEL_N_ESTIMATORS'] + " :")
+            .appendField(new Blockly.FieldNumber(100, 1, null, 1), 'PARAM_N_ESTIMATORS')
+            .appendField(" " + Blockly.Msg['MODEL_MAX_DEPTH'] + " :")
+            .appendField(new Blockly.FieldNumber(5, 0, null, 1), 'PARAM_MAX_DEPTH')
+            .appendField(" " + Blockly.Msg['MODEL_MIN_SAMPLES_SPLIT'] + " :")
+            .appendField(new Blockly.FieldNumber(2, 2, null, 1), 'PARAM_MIN_SAMPLES_SPLIT');
+        this.paramCount_ = 1;
+        break;
+      case 'MLPClassifier':
+        this.appendValueInput('PARAM0')
+            .appendField(" " + Blockly.Msg['MODEL_HIDDEN_LAYER_SIZES'] + " :");
+        this.appendDummyInput('PARAM1')
+            .appendField(" " + Blockly.Msg['MODEL_ACTIVATION'] + " :")
+            .appendField(new Blockly.FieldDropdown([
+              [Blockly.Msg['MODEL_ACTIVATION_LOGISTIC'], 'logistic'],
+              [Blockly.Msg['MODEL_ACTIVATION_TANH'], 'tanh'],
+              [Blockly.Msg['MODEL_ACTIVATION_RELU'], 'relu']
+            ]), 'PARAM_ACTIVATION')
+            .appendField(" " + Blockly.Msg['MODEL_BATCH_SIZE'] + " :")
+            .appendField(new Blockly.FieldNumber(200, 1, null, 1), 'PARAM_BATCH_SIZE');
+        this.paramCount_ = 2;
+        break;
+      case 'SGDClassifier':
+        this.appendDummyInput('PARAM0')
+            .appendField(" " + Blockly.Msg['MODEL_LOSS'] + " :")
+            .appendField(new Blockly.FieldDropdown([
+              [Blockly.Msg['MODEL_LOSS_HINGE'], 'hinge'],
+              [Blockly.Msg['MODEL_LOSS_MODIFIED_HUBER'], 'modified_huber'],
+              [Blockly.Msg['MODEL_LOSS_LOG'], 'log']
+            ]), 'PARAM_LOSS')
+            .appendField(" " + Blockly.Msg['MODEL_PENALTY'] + " :")
+            .appendField(new Blockly.FieldDropdown([
+              [Blockly.Msg['MODEL_PENALTY_L2'], 'l2'],
+              [Blockly.Msg['MODEL_PENALTY_L1'], 'l1']
+            ]), 'PARAM_PENALTY');
+        this.paramCount_ = 1;
+        break;
+    }
+  }
+};
+
+Blockly.Blocks["model_create_regression"] = {
+  /**
+   * Block for creating regression model.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    var MODEL =
+    [
+      [Blockly.Msg['MODEL_KNN'], 'KNeighborsRegressor'],
+      [Blockly.Msg['MODEL_LINEAR_SVM'], 'LinearSVR'],
+      [Blockly.Msg['MODEL_DECISION_TREE'], 'DecisionTreeRegressor'],
+      [Blockly.Msg['MODEL_RANDOM_FOREST'], 'RandomForestRegressor'],
+      [Blockly.Msg['MODEL_MLP'], 'MLPRegressor'],
+      [Blockly.Msg['MODEL_SGD'], 'SGDRegressor']
+    ];
+    this.setStyle('model_blocks');
+    var modelMenu = new Blockly.FieldDropdown(MODEL, function(value) {
+      this.getSourceBlock().updateParameters_(value);
+    });
+    this.appendDummyInput()
+        .appendField(Blockly.Msg['MODEL_CREATE'])
+        .appendField(modelMenu, 'MODEL')
+        .appendField(Blockly.Msg['MODEL_REGRESSION']);
+    this.setInputsInline(true);
+    this.setOutput(true);
+    this.setTooltip(Blockly.Msg['MODEL_CREATE_REGRESSION_TOOLTIP']);
+    this.paramCount_ = 0;
+  },
+  updateParameters_: function(model) {
+    for (var x = 0; x < this.paramCount_; x++) {
+      this.removeInput('PARAM' + x);
+    }
+    switch (model) {
+      case 'KNeighborsRegressor':
+        this.appendDummyInput('PARAM0')
+            .appendField(" k :")
+            .appendField(new Blockly.FieldNumber(5, 1, 10000, 1), 'PARAM_K')
+            .appendField(" " + Blockly.Msg['MODEL_WEIGHT'] + " :")
+            .appendField(new Blockly.FieldDropdown([
+              [Blockly.Msg['MODEL_WEIGHT_UNIFORM'], 'uniform'],
+              [Blockly.Msg['MODEL_WEIGHT_DISTANCE'], 'distance']
+            ]), 'PARAM_WEIGHTS')
+            .appendField(" " + Blockly.Msg['MODEL_ALGORITHM'] + " :")
+            .appendField(new Blockly.FieldDropdown([
+              [Blockly.Msg['MODEL_ALGORITHM_AUTO'], 'auto'],
+              [Blockly.Msg['MODEL_ALGORITHM_BALL_TREE'], 'ball_tree'],
+              [Blockly.Msg['MODEL_ALGORITHM_KD_TREE'], 'kd_tree'],
+              [Blockly.Msg['MODEL_ALGORITHM_BRUTE'], 'brute'],
+            ]), 'PARAM_ALGORITHM');
+        this.paramCount_ = 1;
+        break;
+      case 'LinearSVR':
+        this.appendDummyInput('PARAM0')
+            .appendField(" " + Blockly.Msg['MODEL_C'] + " :")
+            .appendField(new Blockly.FieldNumber(1, 0.0001, null, 0.0001), 'PARAM_C');
+        this.paramCount_ = 1;
+        break;
+      case 'DecisionTreeRegressor':
+        this.appendDummyInput('PARAM0')
+            .appendField(" " + Blockly.Msg['MODEL_MAX_DEPTH'] + " :")
+            .appendField(new Blockly.FieldNumber(5, 0, null, 1), 'PARAM_MAX_DEPTH')
+            .appendField(" " + Blockly.Msg['MODEL_MIN_SAMPLES_SPLIT'] + " :")
+            .appendField(new Blockly.FieldNumber(2, 2, null, 1), 'PARAM_MIN_SAMPLES_SPLIT');
+        this.paramCount_ = 1;
+        break;
+      case 'RandomForestRegressor':
+        this.appendDummyInput('PARAM0')
+            .appendField(" " + Blockly.Msg['MODEL_N_ESTIMATORS'] + " :")
+            .appendField(new Blockly.FieldNumber(100, 1, null, 1), 'PARAM_N_ESTIMATORS')
+            .appendField(" " + Blockly.Msg['MODEL_MAX_DEPTH'] + " :")
+            .appendField(new Blockly.FieldNumber(5, 0, null, 1), 'PARAM_MAX_DEPTH')
+            .appendField(" " + Blockly.Msg['MODEL_MIN_SAMPLES_SPLIT'] + " :")
+            .appendField(new Blockly.FieldNumber(2, 2, null, 1), 'PARAM_MIN_SAMPLES_SPLIT');
+        this.paramCount_ = 1;
+        break;
+      case 'MLPRegressor':
+        this.appendValueInput('PARAM0')
+            .appendField(" " + Blockly.Msg['MODEL_HIDDEN_LAYER_SIZES'] + " :");
+        this.appendDummyInput('PARAM1')
+            .appendField(" " + Blockly.Msg['MODEL_ACTIVATION'] + " :")
+            .appendField(new Blockly.FieldDropdown([
+              [Blockly.Msg['MODEL_ACTIVATION_LOGISTIC'], 'logistic'],
+              [Blockly.Msg['MODEL_ACTIVATION_TANH'], 'tanh'],
+              [Blockly.Msg['MODEL_ACTIVATION_RELU'], 'relu']
+            ]), 'PARAM_ACTIVATION')
+            .appendField(" " + Blockly.Msg['MODEL_BATCH_SIZE'] + " :")
+            .appendField(new Blockly.FieldNumber(200, 1, null, 1), 'PARAM_BATCH_SIZE');
+        this.paramCount_ = 2;
+        break;
+      case 'SGDRegressor':
+        this.appendDummyInput('PARAM0')
+            .appendField(" " + Blockly.Msg['MODEL_LOSS'] + " :")
+            .appendField(new Blockly.FieldDropdown([
+              [Blockly.Msg['MODEL_LOSS_SQUARED_LOSS'], 'squared_loss'],
+              [Blockly.Msg['MODEL_LOSS_HUBER'], 'huber'],
+              [Blockly.Msg['MODEL_LOSS_EPSILON_INSENSITIVE'], 'epsilon_insensitive']
+            ]), 'PARAM_LOSS')
+            .appendField(" " + Blockly.Msg['MODEL_PENALTY'] + " :")
+            .appendField(new Blockly.FieldDropdown([
+              [Blockly.Msg['MODEL_PENALTY_L2'], 'l2'],
+              [Blockly.Msg['MODEL_PENALTY_L1'], 'l1']
+            ]), 'PARAM_PENALTY');
+        this.paramCount_ = 1;
+        break;
+    }
+  }
+};
---
+++ generators/python/model.js
@@ -0,0 +1,218 @@
+/**
+ * @license
+ * Copyright 2021 PAIA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Generating Python for model blocks.
+ * @author jason53415@gmail.com (You-Cheng Xiao)
+ */
+'use strict';
+
+goog.provide('Blockly.Python.Model');
+
+goog.require('Blockly.Python');
+
+Blockly.Python['model_create_classification'] = function(block) {
+  // Create a classification model.
+  var model = block.getFieldValue('MODEL');
+  switch (model) {
+    case 'KNeighborsClassifier':
+      Blockly.Python.definitions_['import_neighbors'] = 'from sklearn import neighbors';
+      var k = block.getFieldValue('PARAM_K');
+      var weights = block.getFieldValue('PARAM_WEIGHTS');
+      var algorithm = block.getFieldValue('PARAM_ALGORITHM');
+      var code = "neighbors.KNeighborsClassifier(" + k + ", weights='" + weights + "', algorithm='" + algorithm + "')";
+      break;
+    case 'LinearSVC':
+      Blockly.Python.definitions_['import_svm'] = 'from sklearn import svm';
+      var penalty = block.getFieldValue('PARAM_PENALTY');
+      var loss = block.getFieldValue('PARAM_LOSS');
+      var c = block.getFieldValue('PARAM_C');
+      var code = "svm.LinearSVC('" + penalty + "', '" + loss + "', C=" + c + ")";
+      break;
+    case 'DecisionTreeClassifier':
+      Blockly.Python.definitions_['import_tree'] = 'from sklearn import tree';
+      var max_depth = block.getFieldValue('PARAM_MAX_DEPTH');
+      var min_samples_split = block.getFieldValue('PARAM_MIN_SAMPLES_SPLIT');
+      if (min_samples_split == 0) {
+        min_samples_split = "None";
+      }
+      var code = "tree.DecisionTreeClassifier(max_depth=" + max_depth + ", min_samples_split=" + min_samples_split + ")";
+      break;
+    case 'RandomForestClassifier':
+      Blockly.Python.definitions_['import_ensemble'] = 'from sklearn import ensemble';
+      var n = block.getFieldValue('PARAM_N_ESTIMATORS');
+      var max_depth = block.getFieldValue('PARAM_MAX_DEPTH');
+      var min_samples_split = block.getFieldValue('PARAM_MIN_SAMPLES_SPLIT');
+      if (min_samples_split == 0) {
+        min_samples_split = "None";
+      }
+      var code = "ensemble.RandomForestClassifier(" + n + ", max_depth=" + max_depth + ", min_samples_split=" + min_samples_split + ")";
+      break;
+    case 'MLPClassifier':
+      Blockly.Python.definitions_['import_neural_network'] = 'from sklearn import neural_network';
+      var hidden_layer_sizes = Blockly.Python.valueToCode(block, 'PARAM0',
+          Blockly.Python.ORDER_COLLECTION) || '[1]';
+      var activation = block.getFieldValue('PARAM_ACTIVATION');
+      var batch_size = block.getFieldValue('PARAM_BATCH_SIZE');
+      var code = "neural_network.MLPClassifier(" + hidden_layer_sizes + ", '" + activation + "', batch_size=" + batch_size + ")";
+      break;
+    case 'SGDClassifier':
+      Blockly.Python.definitions_['import_linear_model'] = 'from sklearn import linear_model';
+      var loss = block.getFieldValue('PARAM_LOSS');
+      var penalty = block.getFieldValue('PARAM_PENALTY');
+      var code = "linear_model.SGDClassifier('" + loss + "', penalty='" + penalty + "')";
+      break;
+  }
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['model_create_regression'] = function(block) {
+  // Create a regression model.
+  var model = block.getFieldValue('MODEL');
+  switch (model) {
+    case 'KNeighborsRegressor':
+      Blockly.Python.definitions_['import_neighbors'] = 'from sklearn import neighbors';
+      var k = block.getFieldValue('PARAM_K');
+      var weights = block.getFieldValue('PARAM_WEIGHTS');
+      var algorithm = block.getFieldValue('PARAM_ALGORITHM');
+      var code = "neighbors.KNeighborsRegressor(" + k + ", weights='" + weights + "', algorithm='" + algorithm + "')";
+      break;
+    case 'LinearSVR':
+      Blockly.Python.definitions_['import_svm'] = 'from sklearn import svm';
+      var c = block.getFieldValue('PARAM_C');
+      var code = "svm.LinearSVR(C=" + c + ")";
+      break;
+    case 'DecisionTreeRegressor':
+      Blockly.Python.definitions_['import_tree'] = 'from sklearn import tree';
+      var max_depth = block.getFieldValue('PARAM_MAX_DEPTH');
+      var min_samples_split = block.getFieldValue('PARAM_MIN_SAMPLES_SPLIT');
+      if (min_samples_split == 0) {
+        min_samples_split = "None";
+      }
+      var code = "tree.DecisionTreeRegressor(max_depth=" + max_depth + ", min_samples_split=" + min_samples_split + ")";
+      break;
+    case 'RandomForestRegressor':
+      Blockly.Python.definitions_['import_ensemble'] = 'from sklearn import ensemble';
+      var n = block.getFieldValue('PARAM_N_ESTIMATORS');
+      var max_depth = block.getFieldValue('PARAM_MAX_DEPTH');
+      var min_samples_split = block.getFieldValue('PARAM_MIN_SAMPLES_SPLIT');
+      if (min_samples_split == 0) {
+        min_samples_split = "None";
+      }
+      var code = "ensemble.RandomForestRegressor(" + n + ", max_depth=" + max_depth + ", min_samples_split=" + min_samples_split + ")";
+      break;
+    case 'MLPRegressor':
+      Blockly.Python.definitions_['import_neural_network'] = 'from sklearn import neural_network';
+      var hidden_layer_sizes = Blockly.Python.valueToCode(block, 'PARAM0',
+          Blockly.Python.ORDER_COLLECTION) || '[1]';
+      var activation = block.getFieldValue('PARAM_ACTIVATION');
+      var batch_size = block.getFieldValue('PARAM_BATCH_SIZE');
+      var code = "neural_network.MLPRegressor(" + hidden_layer_sizes + ", '" + activation + "', batch_size=" + batch_size + ")";
+      break;
+    case 'SGDRegressor':
+      Blockly.Python.definitions_['import_linear_model'] = 'from sklearn import linear_model';
+      var loss = block.getFieldValue('PARAM_LOSS');
+      var penalty = block.getFieldValue('PARAM_PENALTY');
+      var code = "linear_model.SGDRegressor('" + loss + "', penalty='" + penalty + "')";
+      break;
+  }
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['model_train'] = function(block) {
+  // Train a model.
+  var x = Blockly.Python.valueToCode(block, 'X',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var y = Blockly.Python.valueToCode(block, 'Y',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var model = Blockly.Python.valueToCode(block, 'MODEL',
+      Blockly.Python.ORDER_NONE) || 'None';
+  var code = model + '.fit(' + x + ', ' + y + ')\n';
+  return code;
+};
+
+Blockly.Python['model_predict'] = function(block) {
+  // Use model to predict.
+  var model = Blockly.Python.valueToCode(block, 'MODEL',
+      Blockly.Python.ORDER_NONE) || 'None';
+  var x = Blockly.Python.valueToCode(block, 'X',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var code = model + '.predict(' + x + ').tolist()';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['model_evaluate_classification'] = function(block) {
+  // Evaluate classification results.
+  Blockly.Python.definitions_['import_metrics'] = 'from sklearn import metrics';
+  var y_true = Blockly.Python.valueToCode(block, 'TRUE',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var y_pred = Blockly.Python.valueToCode(block, 'PRED',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var code = 'metrics.accuracy_score(' + y_true + ', ' + y_pred + ')';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['model_evaluate_regression'] = function(block) {
+  // Evaluate regression results.
+  Blockly.Python.definitions_['import_metrics'] = 'from sklearn import metrics';
+  var y_true = Blockly.Python.valueToCode(block, 'TRUE',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var y_pred = Blockly.Python.valueToCode(block, 'PRED',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var func = block.getFieldValue('FUNC');
+  switch (func) {
+    case 'R2':
+      var code = 'metrics.r2_score(' + y_true + ', ' + y_pred + ')';
+      break;
+    case 'MAE':
+      var code = 'metrics.mean_absolute_error(' + y_true + ', ' + y_pred + ')';
+      break;
+    case 'MSE':
+      var code = 'metrics.mean_squared_error(' + y_true + ', ' + y_pred + ')';
+      break;
+  }
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['model_train_test_split'] = function(block) {
+  // Split training data and testing data.
+  Blockly.Python.definitions_['import_model_selection'] = 'from sklearn import model_selection';
+  var x = Blockly.Python.valueToCode(block, 'X',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var y = Blockly.Python.valueToCode(block, 'Y',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var test_size = Blockly.Python.valueToCode(block, 'TEST_SIZE',
+      Blockly.Python.ORDER_ATOMIC) || '0';
+  var shuffle = block.getFieldValue('SHUFFLE');
+  var train_data = Blockly.Python.valueToCode(block, 'TRAIN_DATA',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var test_data = Blockly.Python.valueToCode(block, 'TEST_DATA',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var train_target = Blockly.Python.valueToCode(block, 'TRAIN_TARGET',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var test_target = Blockly.Python.valueToCode(block, 'TEST_TARGET',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var code = train_data + ', ' + test_data + ', ' + train_target + ', ' + test_target +
+             ' = model_selection.train_test_split(' + x + ', ' + y + ', test_size=' + test_size + ', shuffle=' + shuffle + ')\n';
+  return code;
+};
+
+Blockly.Python['model_k_fold'] = function(block) {
+  // k-fold cross-validation.
+  Blockly.Python.definitions_['import_model_selection'] = 'from sklearn import model_selection';
+  Blockly.Python.definitions_['import_np'] = 'import numpy as np';
+  var x = Blockly.Python.valueToCode(block, 'X',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var y = Blockly.Python.valueToCode(block, 'Y',
+      Blockly.Python.ORDER_COLLECTION) || '[]';
+  var k = Blockly.Python.valueToCode(block, 'K',
+      Blockly.Python.ORDER_ATOMIC) || '2';
+  var shuffle = block.getFieldValue('SHUFFLE');
+  var code = '[[np.array(' + x + ')[index1].tolist(), np.array(' + x + ')[index2].tolist(), ' +
+             'np.array(' + y + ')[index1].tolist(), np.array(' + y + ')[index2].tolist()] ' +
+             'for index1, index2 in model_selection.KFold(n_splits=' + k + ', shuffle=' + shuffle + ').split(' + x + ', ' + y + ')]';
+  return [code, Blockly.Python.ORDER_COLLECTION];
+};
