c8a677ccfd632eb9b71075a8a26746192289ae1f
You-Cheng Xiao <jason53415@gmail.com>
2021-11-03 08:47:13 +0800

    Customize MLGame blocks

---
+++ blocks/mlgame.js
@@ -0,0 +1,379 @@
+/**
+ * @license
+ * Copyright 2021 PAIA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview MLGame blocks for Blockly.
+ * @author jason53415@gmail.com (You-Cheng Xiao)
+ */
+'use strict';
+
+goog.provide('Blockly.Constants.MLGame');
+
+goog.require('Blockly');
+goog.require('Blockly.Blocks');
+goog.require('Blockly.FieldDropdown');
+goog.require('Blockly.FieldLabel');
+goog.require('Blockly.Mutator');
+
+Blockly.Blocks['mlplay_class'] = {
+  /**
+   * Block for defining the MLPlay class.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": "MLPlay %1 %{BKY_MLPLAY_CLASS_INITIALIZE} %2 %{BKY_MLPLAY_CLASS_UPDATE} %3 %{BKY_MLPLAY_CLASS_RESET} %4",
+      "args0": [
+        {
+          "type": "input_dummy",
+          "align": "CENTRE"
+        },
+        {
+          "type": "input_statement",
+          "name": "INIT",
+          "align": "RIGHT"
+        },
+        {
+          "type": "input_statement",
+          "name": "UPDATE",
+          "align": "RIGHT"
+        },
+        {
+          "type": "input_statement",
+          "name": "RESET",
+          "align": "RIGHT"
+        }
+      ],
+      "style": "mlgame_blocks",
+      "tooltip": "%{BKY_MLPLAY_CLASS_TOOLTIP}",
+      "helpUrl": ""
+    });
+  },
+  onchange: function(_e) {
+    if (!this.workspace.isDragging || this.workspace.isDragging()) {
+      return;  // Don't change state at the start of a drag.
+    }
+    // Is there more than one block?
+    var blockNum = 0;
+    var topBlocks = this.workspace.getTopBlocks();
+    for (var i = 0; i < topBlocks.length; i++) {
+      if (topBlocks[i].type == this.type) {
+        blockNum++;
+      }
+    }
+    if (blockNum > 1 && !this.isInFlyout) {
+      this.setWarningText(Blockly.Msg['MLPLAY_CLASS_WARNING']);
+      this.setEnabled(false);
+    } else {
+      this.setWarningText(null);
+      this.setEnabled(true);
+    }
+  }
+};
+
+Blockly.Blocks['mlplay_init_info'] = {
+  /**
+   * Block for getting initial information.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    if ('MLPLAY_INIT_INFO_OPTIONS' in Blockly.Msg) {
+      this.appendDummyInput()
+          .appendField(Blockly.Msg['MLPLAY_INIT_INFO'])
+          .appendField(new Blockly.FieldDropdown(Blockly.Msg['MLPLAY_INIT_INFO_OPTIONS']), "FIELD");
+      this.setStyle('mlgame_blocks');
+      this.setOutput(true);
+      this.setTooltip(Blockly.Msg['MLPLAY_INIT_INFO_TOOLTIP']);
+    } else {
+      this.appendDummyInput()
+          .appendField(Blockly.Msg['MLPLAY_INIT_INFO'])
+          .appendField(new Blockly.FieldDropdown([["", ""]]), "FIELD");
+      this.setStyle('mlgame_blocks');
+      this.setOutput(true);
+      this.setTooltip(Blockly.Msg['MLPLAY_INIT_INFO_TOOLTIP']);
+      this.setEnabled(false);
+    }
+  },
+  onchange: function(_e) {
+    if (!this.workspace.isDragging || this.workspace.isDragging()) {
+      return;  // Don't change state at the start of a drag.
+    }
+    //
+    var legal = false;
+    var block = this;
+    var parentBlock = this.getParent();
+    while (parentBlock) {
+      if (parentBlock.type == 'mlplay_class') {
+        var input = parentBlock.getInputWithBlock(block);
+        if (input.name == "INIT") {
+          legal = true;
+        }
+        break;
+      }
+      block = parentBlock;
+      parentBlock = block.getParent();
+    }
+    if (legal) {
+      this.setWarningText(null);
+      this.setEnabled(true);
+    } else if (!this.isInFlyout) {
+      this.setWarningText(Blockly.Msg['MLPLAY_INIT_INFO_WARNING']);
+      this.setEnabled(false);
+    }
+  }
+};
+
+Blockly.Blocks['mlplay_player_status'] = {
+  /**
+   * Block for checking game status.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    if ('MLPLAY_PLAYER_STATUS_OPTIONS' in Blockly.Msg) {
+      this.appendDummyInput()
+          .appendField(Blockly.Msg['MLPLAY_PLAYER_STATUS'])
+          .appendField(new Blockly.FieldDropdown(Blockly.Msg['MLPLAY_PLAYER_STATUS_OPTIONS']), "STATUS");
+      this.setStyle('mlgame_blocks');
+      this.setOutput(true);
+      this.setTooltip(Blockly.Msg['MLPLAY_PLAYER_STATUS_TOOLTIP']);
+    } else {
+      this.appendDummyInput()
+          .appendField(Blockly.Msg['MLPLAY_PLAYER_STATUS'])
+          .appendField(new Blockly.FieldDropdown([["", ""]]), "STATUS");
+      this.setStyle('mlgame_blocks');
+      this.setOutput(true);
+      this.setTooltip(Blockly.Msg['MLPLAY_PLAYER_STATUS_TOOLTIP']);
+      this.setEnabled(false);
+    }
+  }
+};
+
+Blockly.Blocks['mlplay_game_status'] = {
+  /**
+   * Block for checking game status.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg['MLPLAY_GAME_STATUS'])
+        .appendField(new Blockly.FieldDropdown(Blockly.Msg['MLPLAY_GAME_STATUS_OPTIONS']), "STATUS");
+    this.setStyle('mlgame_blocks');
+    this.setOutput(true);
+    this.setTooltip(Blockly.Msg['MLPLAY_GAME_STATUS_TOOLTIP']);
+  }
+};
+
+Blockly.Blocks['mlplay_get_info'] = {
+  /**
+   * Block for getting scene information.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg['MLPLAY_GET_INFO'])
+        .appendField(new Blockly.FieldDropdown(Blockly.Msg['MLPLAY_GET_INFO_OPTIONS']), "FIELD");
+    this.setStyle('mlgame_blocks');
+    this.setOutput(true);
+    this.setTooltip(Blockly.Msg['MLPLAY_GET_INFO_TOOLTIP']);
+  },
+  onchange: function(_e) {
+    if (!this.workspace.isDragging || this.workspace.isDragging()) {
+      return;  // Don't change state at the start of a drag.
+    }
+    //
+    var legal = false;
+    var block = this;
+    var parentBlock = this.getParent();
+    while (parentBlock) {
+      if (parentBlock.type == 'mlplay_class') {
+        var input = parentBlock.getInputWithBlock(block);
+        if (input.name == "UPDATE") {
+          legal = true;
+        }
+        break;
+      }
+      block = parentBlock;
+      parentBlock = block.getParent();
+    }
+    if (legal) {
+      this.setWarningText(null);
+      this.setEnabled(true);
+    } else if (!this.isInFlyout) {
+      this.setWarningText(Blockly.Msg['MLPLAY_GET_INFO_WARNING']);
+      this.setEnabled(false);
+    }
+  }
+};
+
+Blockly.Blocks['mlplay_get_constant'] = {
+  /**
+   * Block for getting game constants.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg['MLPLAY_GET_CONSTANT'])
+        .appendField(new Blockly.FieldDropdown(Blockly.Msg['MLPLAY_GET_CONSTANT_OPTIONS']), "CONSTANT");
+    this.setStyle('mlgame_blocks');
+    this.setOutput(true);
+    this.setTooltip(Blockly.Msg['MLPLAY_GET_CONSTANT_TOOLTIP']);
+  }
+};
+
+Blockly.Blocks['mlplay_return_action'] = {
+  /**
+   * Block for returning next action.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg['MLPLAY_RETURN_ACTION'])
+        .appendField(new Blockly.FieldDropdown(Blockly.Msg['MLPLAY_RETURN_ACTION_OPTIONS']), "ACTION");
+    this.setStyle('mlgame_blocks');
+    this.setOutput(false);
+    this.setPreviousStatement(true);
+    this.setTooltip(Blockly.Msg['MLPLAY_RETURN_ACTION_TOOLTIP']);
+  },
+  onchange: function(_e) {
+    if (!this.workspace.isDragging || this.workspace.isDragging()) {
+      return;  // Don't change state at the start of a drag.
+    }
+    //
+    var legal = false;
+    var block = this;
+    var parentBlock = this.getParent();
+    while (parentBlock) {
+      if (parentBlock.type == 'mlplay_class') {
+        var input = parentBlock.getInputWithBlock(block);
+        if (input.name == "UPDATE") {
+          legal = true;
+        }
+        break;
+      }
+      block = parentBlock;
+      parentBlock = block.getParent();
+    }
+    if (legal) {
+      this.setWarningText(null);
+      this.setEnabled(true);
+    } else if (!this.isInFlyout) {
+      this.setWarningText(Blockly.Msg['MLPLAY_RETURN_ACTION_WARNING']);
+      this.setEnabled(false);
+    }
+  }
+};
+
+Blockly.Blocks['mlplay_return_mazecar_action'] = {
+  /**
+   * Block for returning Maze_Car action.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": "%{BKY_MLPLAY_RETURN_MAZECAR_ACTION}",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "LEFT_RPM",
+          "check": "Number",
+          "align": "RIGHT",
+        },
+        {
+          "type": "input_value",
+          "name": "RIGHT_RPM",
+          "check": "Number",
+          "align": "RIGHT"
+        }
+      ],
+      "style": "mlgame_blocks",
+      "previousStatement": null,
+      "tooltip": "%{BKY_MLPLAY_RETURN_ACTION_TOOLTIP}",
+      "helpUrl": ""
+    });
+  },
+  onchange: function(_e) {
+    if (!this.workspace.isDragging || this.workspace.isDragging()) {
+      return;  // Don't change state at the start of a drag.
+    }
+    //
+    var legal = false;
+    var block = this;
+    var parentBlock = this.getParent();
+    while (parentBlock) {
+      if (parentBlock.type == 'mlplay_class') {
+        var input = parentBlock.getInputWithBlock(block);
+        if (input.name == "UPDATE") {
+          legal = true;
+        }
+        break;
+      }
+      block = parentBlock;
+      parentBlock = block.getParent();
+    }
+    if (legal) {
+      this.setWarningText(null);
+      this.setEnabled(true);
+    } else if (!this.isInFlyout) {
+      this.setWarningText(Blockly.Msg['MLPLAY_RETURN_ACTION_WARNING']);
+      this.setEnabled(false);
+    }
+  }
+};
+
+Blockly.Blocks['mlplay_is_key_pressed'] = {
+  /**
+   * Block for checking a key is pressed or not.
+   * @this {Blockly.Block}
+   */
+  init: function() {
+    this.key_types_ = [
+      ['%{BKY_MLPLAY_ARROW_KEYS}', 'arrow'],
+      ['%{BKY_MLPLAY_NUMBER_KEYS}', 'number'],
+      ['%{BKY_MLPLAY_ALPHA_KEYS}', 'alpha']
+    ];
+    this.arrow_keys_ = [
+      ["\u2191", 'up'],
+      ["\u2193", 'down'],
+      ["\u2190", 'left'],
+      ["\u2192", 'right']
+    ];
+    this.number_keys_ = Array.apply(null, Array(10)).map(
+        function(_, i) {
+          return [String.fromCharCode(i + 48), String.fromCharCode(i + 48)];
+        });
+    this.alpha_keys_ = Array.apply(null, Array(26)).map(
+        function(_, i) {
+          return [String.fromCharCode(i + 65), String.fromCharCode(i + 65)];
+        });
+    this.appendDummyInput('FIELDS')
+        .appendField(new Blockly.FieldDropdown(this.key_types_, function(value) {
+          this.getSourceBlock().updateKeys_(value);
+        }), "TYPE")
+        .appendField(new Blockly.FieldDropdown(this.arrow_keys_), "KEY")
+        .appendField(Blockly.Msg['MLPLAY_KEY_PRESSED']);
+    this.setStyle('mlgame_blocks');
+    this.setOutput(true);
+    this.setTooltip(Blockly.Msg['MLPLAY_IS_KEY_PRESSED_TOOLTIP']);
+  },
+  updateKeys_: function(type) {
+    var fields = this.getInput('FIELDS');
+    fields.removeField("KEY");
+    switch (type) {
+      case 'arrow':
+        fields.insertFieldAt(1, new Blockly.FieldDropdown(this.arrow_keys_), "KEY");
+        break;
+      case 'number':
+        fields.insertFieldAt(1, new Blockly.FieldDropdown(this.number_keys_), "KEY");
+        break;
+      case 'alpha':
+        fields.insertFieldAt(1, new Blockly.FieldDropdown(this.alpha_keys_), "KEY");
+        break;
+    }
+  },
+  getDeveloperVariables: function() {
+    return ['_KEYBOARD_ON_PRESSED'];
+  }
+};
---
+++ generators/python/mlgame.js
@@ -0,0 +1,127 @@
+/**
+ * @license
+ * Copyright 2021 PAIA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Generating Python for MLGame blocks.
+ * @author jason53415@gmail.com (You-Cheng Xiao)
+ */
+goog.provide('Blockly.Python.MLGame');
+
+goog.require('Blockly.Python');
+
+Blockly.Python['mlplay_class'] = function(block) {
+  // First, add a 'global' statement for every variable that is not shadowed by
+  // a local parameter.
+  var globals = [];
+  var varName;
+  var workspace = block.workspace;
+  var variables = Blockly.Variables.allUsedVarModels(workspace) || [];
+  for (var i = 0, variable; (variable = variables[i]); i++) {
+    varName = variable.name;
+    if (block.getVars().indexOf(varName) == -1) {
+      globals.push(Blockly.Python.variableDB_.getName(varName,
+          Blockly.VARIABLE_CATEGORY_NAME));
+    }
+  }
+  // Add developer variables.
+  var devVarList = Blockly.Variables.allDeveloperVariables(workspace);
+  for (var i = 0; i < devVarList.length; i++) {
+    globals.push(Blockly.Python.variableDB_.getName(devVarList[i],
+        Blockly.Names.DEVELOPER_VARIABLE_TYPE));
+  }
+
+  globals = globals.length ?
+      Blockly.Python.INDENT + 'global ' + globals.join(', ') + '\n' : '';
+  var init = Blockly.Python.statementToCode(block, 'INIT') || Blockly.Python.PASS;
+  var init_var = "";
+  if ('MLPLAY_INIT_INFO_OPTIONS' in Blockly.Msg) {
+    for (var i = 0; i < Blockly.Msg['MLPLAY_INIT_INFO_OPTIONS'].length; i++) {
+      init_var += (', ' + Blockly.Msg['MLPLAY_INIT_INFO_OPTIONS'][i][1]);
+    }
+  }
+  var update = Blockly.Python.statementToCode(block, 'UPDATE') || Blockly.Python.PASS;
+  var reset = Blockly.Python.statementToCode(block, 'RESET') || Blockly.Python.PASS;
+  var code = 'class MLPlay:\n' + Blockly.Python.prefixLines(
+      'def __init__(self' + init_var + '):\n' + globals + init +
+      'def update(self, scene_info):\n' + globals + update +
+      'def reset(self):\n' + globals + reset, Blockly.Python.INDENT);
+  return code;
+};
+
+Blockly.Python['mlplay_init_info'] = function(block) {
+  var code = block.getFieldValue('FIELD');
+  return [code, Blockly.Python.ORDER_STRING_CONVERSION];
+};
+
+Blockly.Python['mlplay_player_status'] = function(block) {
+  var code = '"' + block.getFieldValue('STATUS') + '"';
+  return [code, Blockly.Python.ORDER_STRING_CONVERSION];
+};
+
+Blockly.Python['mlplay_game_status'] = function(block) {
+  var code = '"' + block.getFieldValue('STATUS') + '"';
+  return [code, Blockly.Python.ORDER_STRING_CONVERSION];
+};
+
+Blockly.Python['mlplay_get_info'] = function(block) {
+  var code = block.getFieldValue('FIELD');
+  return [code, Blockly.Python.ORDER_STRING_CONVERSION];
+};
+
+Blockly.Python['mlplay_get_constant'] = function(block) {
+  var code = block.getFieldValue('CONSTANT').split("/").pop();
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['mlplay_return_action'] = function(block) {
+  var action = block.getFieldValue('ACTION');
+  if (action[0] == '[' || action[0] == '{') {
+    var code = 'return ' + action + '\n';
+  } else {
+    var code = 'return "' + action + '"\n';
+  }
+  return code;
+};
+
+Blockly.Python['mlplay_return_mazecar_action'] = function(block) {
+  var left = Blockly.Python.valueToCode(block, 'LEFT_RPM',
+      Blockly.Python.ORDER_NONE) || '0';
+  var right = Blockly.Python.valueToCode(block, 'RIGHT_RPM',
+      Blockly.Python.ORDER_NONE) || '0';
+  var code = "return [{'left_PWM': " + left + ", 'right_PWM': " + right + "}]\n";
+  return code;
+};
+
+Blockly.Python['mlplay_is_key_pressed'] = function(block) {
+  Blockly.Python.definitions_['import_keyboard'] = 'from pynput import keyboard';
+  Blockly.Python.definitions_['import_defaultdict'] = 'from collections import defaultdict';
+  var on_press_func = Blockly.Python.provideFunction_(
+      'on_press',
+      ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '(key):',
+        '  _KEYBOARD_ON_PRESSED[str(key)] = True']);
+  var on_release_func = Blockly.Python.provideFunction_(
+      'on_release',
+      ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '(key):',
+        '  _KEYBOARD_ON_PRESSED[str(key)] = False']);
+  Blockly.Python.provideFunction_(
+      'keyboard_listener',
+      ['_KEYBOARD_ON_PRESSED = defaultdict(bool)',
+        'listener = keyboard.Listener(',
+        '  on_press=' + on_press_func + ',',
+        '  on_release=' + on_release_func + ')',
+        'listener.start()']);
+  var type = block.getFieldValue('TYPE');
+  var key = block.getFieldValue('KEY');
+  if (type == 'arrow') {
+    var code = '_KEYBOARD_ON_PRESSED["Key.' + key + '"]';
+  } else if (type == 'alpha') {
+    var code = '(_KEYBOARD_ON_PRESSED["\'' + key + '\'"] or ' +
+               '_KEYBOARD_ON_PRESSED["\'' + key.toLowerCase() + '\'"])';
+  } else {
+    var code = '_KEYBOARD_ON_PRESSED["\'' + key + '\'"]';
+  }
+  return [code, Blockly.Python.ORDER_RELATIONAL];
+};
